711906b806c22e095be053328a0a0d8a80dadd3e
Colin Woodbury <colin@fosskers.ca>
2023-11-20 10:48:31 +0900

    docs: move the internal note about shorthanding

--- transducers.el
+++ transducers.el
@@ -5,7 +5,7 @@
 ;; Author: Colin Woodbury <colin@fosskers.ca>
 ;; Maintainer: Colin Woodbury <colin@fosskers.ca>
 ;; Created: July 26, 2023
-;; Modified: November 16, 2023
+;; Modified: November 20, 2023
 ;; Version: 1.0.0
 ;; Keywords: lisp
 ;; Homepage: https://git.sr.ht/~fosskers/transducers.el
@@ -16,30 +16,25 @@
 ;;
 ;;; Commentary:
 ;;
-;;  Transducers are an ergonomic and extremely memory-efficient way to process a
-;;  data source. Here "data source" means simple collections like Lists or
-;;  Vectors, but also potentially large files or generators of infinite data.
+;; Transducers are an ergonomic and extremely memory-efficient way to process a
+;; data source. Here "data source" means simple collections like Lists or
+;; Vectors, but also potentially large files or generators of infinite data.
 ;;
-;;  Transducers...
+;; Transducers...
 ;;
-;;  - allow the chaining of operations like map and filter without allocating
-;;    memory between each step.
-;;  - aren't tied to any specific data type; they need only be implemented once.
-;;  - vastly simplify "data transformation code".
-;;  - have nothing to do with "lazy evaluation".
-;;  - are a joy to use!
+;; - allow the chaining of operations like map and filter without allocating
+;;   memory between each step.
+;; - aren't tied to any specific data type; they need only be implemented once.
+;; - vastly simplify "data transformation code".
+;; - have nothing to do with "lazy evaluation".
+;; - are a joy to use!
 ;;
-;;  See the README for examples.
+;; See the README for examples.
 ;;
-;;; Code:
-
-(require 'cl-lib)
-(require 'ring)
-
-;; NOTE
+;; **Usage Note**
 ;;
 ;; This library uses the `read-symbol-shorthands' feature introduced in Emacs
-;; 28. While browsing this file it may appear as if most of the functions are
+;; 28. While browsing the code it may appear as if most of the functions are
 ;; prefixed by `t-', but in fact the symbols are exported with the full prefix
 ;; `transducers-'. Every user is then free to abbreviate the function symbols as
 ;; they wish in their own files, just like import systems in other languages
@@ -48,7 +43,12 @@
 ;; To enable this abbreviation in your own Emacs Lisp files, interactively call
 ;; either `add-file-local-variable-prop-line' or `add-file-local-variable' and
 ;; set `read-symbol-shorthands' to a value like (("t-" . "transducers-")). You
-;; can see an example of this in the top line of this file.
+;; can see an example of this in the top line of this file (transducers.el).
+;;
+;;; Code:
+
+(require 'cl-lib)
+(require 'ring)
 
 ;; --- Utilities --- ;;
 
